da1166791403774de2a85cd1490980aa8ebe3bc4
Javier Alvarez <alvarez15@avaya.com>
2018-09-07 16:12:44 -0300

    Nuevo Proyecto WPF

--- WPFApp/WpfApp2/MainWindow.xaml.cs
+++ WPFApp/WpfApp2/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFApp
+namespace WpfApp2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,11 +23,6 @@ namespace WPFApp
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click()
-        {
-
         }
     }
 }

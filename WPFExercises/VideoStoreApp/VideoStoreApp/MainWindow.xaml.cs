f52a19c7b496d8ecf8870e9b0fe186264924bb65
Javier Alvarez <alvarez15@avaya.com>
2018-09-17 16:14:19 -0300

    asd

---
+++ WPFExercises/VideoStoreApp/VideoStoreApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoStoreApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Members members = new Members();
+        NotificationManager manager = new NotificationManager();
+        public MainWindow()
+        {
+            DataContext = members;
+
+            members.MemberAddedHandler += manager.OnMemberAdded;
+            InitializeComponent();
+         
+           lvUsers.ItemsSource = members.List;
+            
+            
+
+
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (members.Name != null && members.AuxName != null)
+            {
+                members.AddMember(members.Name, members.AuxName);
+                
+            }
+            
+            NameTextBox.Clear();
+            MailTextBox.Clear();
+            members.Name = null;
+            members.AuxName = null;
+           
+
+        }
+    }
+}
---
+++ WPFExercises/VideoStoreApp/VideoStoreApp/Members.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace VideoStoreApp
+{
+    public class Members : INotifyPropertyChanged
+    {
+
+        private string _name;
+        private string _auxName;
+        private string _emailAddress;
+
+
+        public String Name
+        {
+            get { return _name; }
+            set
+            {
+                if (Name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+
+        public String AuxName
+        {
+            get { return _auxName; }
+            set
+            {
+                if (AuxName != value)
+                {
+                    _auxName = value;
+                    OnPropertyChanged("AuxName");
+                }
+            }
+        }
+
+        public String EMailAddress { get; set; }
+        public ObservableCollection<Members> List = new ObservableCollection<Members>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public delegate void MemberAdded(object sender, EventArgs args);
+
+        public event MemberAdded MemberAddedHandler;
+
+        public void AddMember(String name, String eMailAddress)
+        {
+            
+            
+            List.Add(new Members(){Name = name, EMailAddress = eMailAddress });
+            OnMemberAdded();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected virtual void OnMemberAdded()
+        {
+            if (MemberAddedHandler != null)
+            {
+                MemberAddedHandler(this, EventArgs.Empty);
+            }
+        }
+
+
+        
+        
+    }
+
+    public class NotificationManager
+    {
+        public void OnMemberAdded(object source, EventArgs args)
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+}
